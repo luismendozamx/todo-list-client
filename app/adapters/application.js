516586e47d01d3bed5078ec6276aee1b326d81d2
Luis Mendoza <luis@mendoza.io>
2016-01-06 10:34:02 -0600

    Adds list model and setup relatinships between list and todos. Remove shouldReloadAll in application adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  namespace: 'api',
-
-  shouldReloadAll: function() {
-    return true;
-  }
+  namespace: 'api'
 });
--- app/models/list.js
+++ app/models/list.js
@@ -3,8 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   // Attributes
   title: DS.attr('string'),
-  completed: DS.attr('boolean'),
 
   // Relationships
-  list: DS.belongsTo('list')
+  todos: DS.hasMany('todos')
 });
