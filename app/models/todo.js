8789abb602c07cd7e20b67e20bcdcc7d8a9fc391
Luis Mendoza <luis@mendoza.io>
2016-01-08 11:55:27 -0600

    Adds description attr to todo model

--- app/models/todo.js
+++ app/models/todo.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   // Attributes
   title: DS.attr('string'),
   completed: DS.attr('boolean'),
+  description: DS.attr('text'),
 
   // Relationships
   list: DS.belongsTo('list')
